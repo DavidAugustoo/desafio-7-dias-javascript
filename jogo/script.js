5453863964f2be2f31d55bfb96527681cddc3674
David Augusto <davidribeiro086@gmail.com>
2022-03-10 16:19:58 -0300

    :sparkles: jogo: verificação do ganhador

--- jogo/script.js
+++ jogo/script.js
@@ -45,3 +45,69 @@ function reset() {
     playing = true;
 }
 
+function renderQuadro() {
+    for(let i in quadro) {
+        let item = document.querySelector(`div[data-item=${i}]`);
+        if(quadro[i] !== '') {
+            item.innerHTML = quadro[i];
+        } else {
+            item.innerHTML = '';
+        }
+    }
+
+    checkGame();
+}
+
+function renderInfo() {
+    document.querySelector('.vez').innerHTML = vez;
+    document.querySelector('.resultado').innerHTML = warning;
+}
+
+function togglePlayer() {
+    vez = vez === 'x' ? 'o' : 'x';
+    renderInfo();
+}
+
+function checkGame() {
+    if(checkWinnerFor('x')) {
+        warning = 'O "x" venceu';
+        playing = false;
+    } else if(checkWinnerFor('o')) {
+        warning = 'O "o" venceu';
+        playing = false;
+    } else if(isFull()) {
+        warning = 'Deu empate';
+        playing = false;
+    }
+}
+
+function checkWinnerFor(i) {
+    let pos = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+
+        'a1,b1,c1',
+        'a2,b2,c2',
+        'a3,b3,c3',
+
+        'a1,b2,c3',
+        'a3,b2,c1'
+    ];
+
+    for(let w in pos) {
+        let pArray = pos[w].split(',');
+        let hasWon = pArray.every(option=>quadro[option] === i);
+        if(hasWon) return true;
+    }
+
+    return false;
+}
+function isFull() {
+    for(let i in quadro) {
+        if(quadro[i] === '') {
+            return false;
+        }
+    }
+    return true;
+}
